686fee9668423a7846d5b1e5b9d06b90f09baae6
Carlos Albaladejo Pérez <karlozalb@gmail.com>
2018-05-22 22:48:30 +0200

    A room!

---
+++ RoomEscape/Source/RoomEscape/PositionReporter.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositionReporter.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+
+	FString name = GetOwner()->GetName();
+
+	//FVector position = GetOwner()->GetTransform().GetLocation().ToString();
+
+	FString objectPos = GetOwner()->GetTransform().GetLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("Soy %s y estoy en %s"), *name, *objectPos);
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
